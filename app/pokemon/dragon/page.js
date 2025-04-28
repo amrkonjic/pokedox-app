17ced3eedd7e228e63f307f5369ca085f5a278db
amrkonjic <mrkonjicanela@gmail.com>
2025-04-28 15:37:51 +0200

    implementation of dragon and ghost pages

--- app/pokemon/dragon/page.js
+++ app/pokemon/dragon/page.js
@@ -1,3 +1,29 @@
-export default function DragonPokemon() {
-    return <h1>Popis Dragon Pokemona</h1>;
+import NotFound from "@/app/not-found";
+
+export default async function DragonPokemon() {
+
+  const res = await fetch("https://pokeapi.co/api/v2/type/dragon/");
+  if(!res.ok){
+    return NotFound;
+  }
+  const data = await res.json();
+  const dragonPokemons = data.pokemon || [];
+  
+
+  console.log(dragonPokemons);
+
+    return (
+      <div>
+        <h1>List of Dragon Pokemons</h1>
+        <ul>
+          {dragonPokemons.map((dragon, index) => {
+            return(
+              <li key={index}>
+                {dragon.pokemon.name}
+              </li>
+            )  
+          })}
+        </ul>
+      </div>
+    );
   }
--- app/pokemon/ghost/page.js
+++ app/pokemon/ghost/page.js
@@ -1,3 +1,28 @@
-export default function GhostPokemon() {
-    return <h1>Popis Ghost Pokemona</h1>;
+import NotFound from "@/app/not-found";
+
+export default async function GhostPokemon() {
+    const res = await fetch("https://pokeapi.co/api/v2/type/ghost/")
+    if(!res.ok){
+      return NotFound;
+    }
+
+    const data = await res.json();
+    const ghostPokemons = data.pokemon || [];
+
+    return(
+      <div>
+         <h1>List of Ghost Pokemons</h1>
+         <ul>
+          {ghostPokemons.map((ghost, index) => {
+            return(
+              <li key={index}>
+                {ghost.pokemon.name}
+              </li>
+            )
+          })}
+         </ul>
+      </div>
+    )
+
   }
+  
