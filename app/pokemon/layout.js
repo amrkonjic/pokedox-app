d47e38af3aa1fdf5e2d62ad6277e4bfd3c136ceb
amrkonjic <mrkonjicanela@gmail.com>
2025-04-30 17:31:25 +0200

    change of page with pagination

--- app/pokemon/layout.js
+++ app/pokemon/layout.js
@@ -2,25 +2,30 @@ import Link from "next/link";
 import BackButton from "../components/BackButton";
 export default function PokemonLayout({ children }) {
     return (
-      <div className='flex'>
-        <aside className='w-60 bg-white shadow-lg p-4'>
-            <h2 className='text-xl font-bold mb-4'>Pokémon Menu</h2>
-            <nav className='space-y-2 mb-4'>
-              <ul>
+      <div className='flex justify-between min-h-screen'>
+        <aside className='w-60 sticky top-0 h-full min-h-screen bg-blue-950 shadow-lg p-4'>
+            <h2 className='text-xl text-gray-50 font-bold mb-4'>Pokémon Menu</h2>
+            <nav className='space-y-10 mb-4'>
+              <ul className="space-y-3 mt-8">
                 <li>
                   <Link
                     href='/pokemon/dragon'
-                    className='block text-gray-700 hover:text-blue-600'
+                    className='block text-gray-50 hover:text-gray-400'
                   >
-                    Dragon Pokémoni
+                    Dragon Pokémons
                   </Link>
                 </li>
                 <li>
                   <Link
                     href='/pokemon/ghost'
-                    className='block text-gray-700 hover:text-blue-600'
+                    className='block text-gray-50 hover:text-gray-400'
                   >
-                    Ghost Pokémoni
+                    Ghost Pokémons
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/pokemon" className="block text-gray-50 hover:text-gray-400">
+                    Search
                   </Link>
                 </li>
               </ul>
@@ -29,7 +34,7 @@ export default function PokemonLayout({ children }) {
               </div>
             </nav>
           </aside>
-        <main className='flex-1 p-4'>{children}</main>
+        <main className='flex-1 flex justify-center'>{children}</main>
       </div>
     );
   }
--- app/pokemon/page/[pageNumber]/page.js
+++ app/pokemon/page/[pageNumber]/page.js
@@ -1,42 +1,46 @@
 import Link from "next/link";
 export default async function PokemonPage({ params }) {
     const { pageNumber } = await params;
-    const brStranice = parseInt(pageNumber, 10) || 1; // ako je krivi parametar
+    const pageNum = parseInt(pageNumber, 10) || 1; // ako je krivi parametar
     const limit = 20;
-    const offset = (brStranice - 1) * limit;
+    const offset = (pageNum - 1) * limit;
    
     const res = await fetch(
       `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     );
     const data = await res.json();
-   
-    console.log(data);
+    const pokemons = data.results || [];
    
     return (
         <main className='p-4'>
-        <h1 className='text-2xl mb-4'>Pokémon – Stranica {brStranice}</h1>
-        <ul className='space-y-1'>
-        {data.results.map(p => (
-        <li key={p.name}>{p.name}</li>
-        ))}
-        </ul>
-        <div className='mt-4 flex gap-2'>
-        {brStranice > 1 && (
-          <Link
-            href={`/pokemon/page/${brStranice - 1}`}
-            className='px-2 py-1 bg-gray-300'
-          >
-            Prethodna
-          </Link>
-        )}
-        <Link
-          href={`/pokemon/page/${brStranice + 1}`}
-          className='px-2 py-1 bg-gray-300'
-        >
-          Iduća
-        </Link>
-      </div>
+            <h1 className='text-2xl mb-4'>Pokémon – Page {pageNum}</h1>
+            <ul className='space-y-1'>
+              {pokemons.map((pokemon, index) => {
+                return (
+                  <li key={index}>
+                    <Link href={`/pokemon/${pokemon.name}`}>
+                      {pokemon.name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className='mt-4 flex gap-2'>
+            {pageNum > 1 && (
+              <Link
+                href={`/pokemon/page/${pageNum - 1}`}
+                className='px-2 py-1 bg-gray-300'
+              >
+                Previous
+              </Link>
+            )}
+            <Link
+              href={`/pokemon/page/${pageNum + 1}`}
+              className='px-2 py-1 bg-gray-300'
+            >
+              Next
+            </Link>
+          </div>
         </main>
-       )
-       ;
+       );
   }
